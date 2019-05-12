c9806d40fe8863e6a4a02840687a8110ddee2538
rossi23891 <rossi23891@gmail.com>
2019-05-12 20:13:59 +0400

    2511

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2511/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task25.task2511;
+
+import java.util.TimerTask;
+
+/* 
+Вооружаемся до зубов!
+*/
+public class Solution extends TimerTask {
+    protected TimerTask original;
+    protected final Thread.UncaughtExceptionHandler handler;
+
+    public Solution(TimerTask original) {
+        if (original == null) {
+            throw new NullPointerException();
+        }
+        this.original = original;
+        this.handler = new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                int len = t.getName().length();
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i <len ; i++) {
+                    sb.append("*");
+                }
+                System.out.println(e.getMessage().replaceAll(t.getName(),sb.toString()));
+
+            }
+        };    //init handler here
+    }
+
+    public void run() {
+        try {
+            original.run();
+        } catch (Throwable cause) {
+            Thread currentThread = Thread.currentThread();
+            handler.uncaughtException(currentThread, new Exception("Blah " + currentThread.getName() + " blah-blah-blah", cause));
+        }
+    }
+
+    public long scheduledExecutionTime() {
+        return original.scheduledExecutionTime();
+    }
+
+    public boolean cancel() {
+        return original.cancel();
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Solution(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                System.out.println(5/0);
+            }
+        }));
+        thread.start();
+    }
+}
