e71cafc64bee2fa2a15c607e422009140efe089c
rossi23891 <rossi23891@gmail.com>
2019-05-30 18:11:43 +0300

    chat 8 semi-done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -10,13 +10,33 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
 
-    private static Map<String,Connection> connectionMap = new ConcurrentHashMap<>();//key - client name, val - connection with this client
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();//key - client name, val - connection with this client
 
     private static class Handler extends Thread {
         Socket socket;
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            connection.send(new Message(MessageType.NAME_REQUEST, "Hi, who are you?"));
+            Message message = connection.receive();
+            if (!message.getType().equals(MessageType.USER_NAME)) {
+                serverHandshake(connection);
+            }
+
+            String userName = message.getData();
+            if (userName.equals("")) {
+                serverHandshake(connection);
+            }
+            if (connectionMap.containsKey(userName)) {
+                serverHandshake(connection);
+            }
+
+            connectionMap.put(userName, connection);
+            connection.send(new Message(MessageType.NAME_ACCEPTED,"You're welcome!"));
+            return userName;
         }
     }
 
@@ -32,14 +52,15 @@ public class Server {
             serverSocket.close();
         }
     }
-    public static void sendBroadcastMessage(Message message){
-            for(Connection connection: connectionMap.values()){
-                try {
-                    connection.send(message);
-                } catch (IOException e) {
-                    System.out.println("Сообщение не отправлено");
-                }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                System.out.println("Сообщение не отправлено");
             }
+        }
     }
 }
 
