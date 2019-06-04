e33c1bc71dde8721e1590b7a67a3274bf2a9e9bc
rossi23891 <rossi23891@gmail.com>
2019-06-04 18:14:17 +0300

    chat semi-20

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiModel.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task30.task3008.client;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClientGuiModel {
+    private final Set<String> allUserNames = new HashSet<>();
+    private String newMessage;
+}
