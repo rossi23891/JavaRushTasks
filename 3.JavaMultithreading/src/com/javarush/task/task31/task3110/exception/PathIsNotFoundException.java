22a422ae6e3983b46bd02e28918b4adfb8f6142f
rossi23891 <rossi23891@gmail.com>
2019-07-02 16:32:53 +0300

    archive 7

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task31.task3110.exception;
+
+public class PathIsNotFoundException extends Exception { // не можем найти путь, куда распаковать архив.или путь к файлу,
+                                                        // который хотим запаковать, или любой другой путь.
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/WrongZipFileException.java
@@ -0,0 +1,4 @@
+package com.javarush.task.task31.task3110.exception;
+
+public class WrongZipFileException extends Exception {// если будет попытка сделать что-нибудь с архивом, который не существует
+}
