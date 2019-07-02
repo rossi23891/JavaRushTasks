744b08685d24775ac850ad42e348edd33a78707e
rossi23891 <rossi23891@gmail.com>
2019-07-02 18:06:53 +0300

    archive 8

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,12 +1,12 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.IOException;
 
 public class Archiver {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
         Operation operation = null;
         do {
             try {
@@ -17,33 +17,21 @@ public class Archiver {
             } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-        } while (operation != (Operation.EXIT));
+
+        } while (operation != Operation.EXIT);
     }
 
-    public static Operation askOperation() throws IOException {
-        Operation operation = null;
-        ConsoleHelper.writeMessage("Выберите операцию:\n" +
-                "0 - упаковать файлы в архив\n" +
-                "1 - добавить файл в архив\n" +
-                "2 - удалить файл из архива\n" +
-                "3 - распаковать архив\n" +
-                "4 - просмотреть содержимое архива\n" +
-                "5 - выход");
 
-        int userOperation = ConsoleHelper.readInt();
-        if (userOperation == Operation.CREATE.ordinal()) {
-            operation = Operation.CREATE;
-        } else if (userOperation == Operation.ADD.ordinal()) {
-            operation = Operation.ADD;
-        } else if (userOperation == Operation.REMOVE.ordinal()) {
-            operation = Operation.REMOVE;
-        } else if (userOperation == Operation.EXTRACT.ordinal()) {
-            operation = Operation.EXTRACT;
-        } else if (userOperation == Operation.CONTENT.ordinal()) {
-            operation = Operation.CONTENT;
-        } else if (userOperation == Operation.EXIT.ordinal()) {
-            operation = Operation.EXIT;
-        }
-        return operation;
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
