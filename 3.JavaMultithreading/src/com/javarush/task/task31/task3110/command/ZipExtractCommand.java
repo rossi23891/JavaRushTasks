77e5cb574a5195d0cf8fa32afbc0805ced3717af
rossi23891 <rossi23891@gmail.com>
2019-07-02 16:27:46 +0300

    archive 7

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task31.task3110.command;
+
+import com.javarush.task.task31.task3110.command.ZipCommand;
+
+public class ZipExtractCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
