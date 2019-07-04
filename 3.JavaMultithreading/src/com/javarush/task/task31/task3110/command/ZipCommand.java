8a545a5640c39696e9b14f9b330bf4f55384dfdf
rossi23891 <rossi23891@gmail.com>
2019-07-04 16:41:35 +0300

    archive 11

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,5 +1,17 @@
 package com.javarush.task.task31.task3110.command;
 
-public abstract class ZipCommand implements Command{
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public abstract class ZipCommand implements Command{
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите путь файла архива");
+        String pathToArchive = ConsoleHelper.readString();
+        //путь файла, где будет создаваться архив
+        Path archivePath = Paths.get(pathToArchive);
+        return new ZipFileManager(archivePath);
+    }
 }
