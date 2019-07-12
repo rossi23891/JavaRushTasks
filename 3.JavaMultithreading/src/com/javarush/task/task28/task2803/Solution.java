4791de82a22dac71f2663824eb039429280acf7f
rossi23891 <rossi23891@gmail.com>
2019-07-12 13:47:35 +0300

    2803

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2803/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task28.task2803;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/* 
+ThreadLocalRandom
+*/
+public class Solution {
+    public static int getRandomIntegerBetweenNumbers(int from, int to) {
+
+        return ThreadLocalRandom.current().nextInt(from, to);
+    }
+
+    public static double getRandomDouble() {
+
+        return ThreadLocalRandom.current().nextDouble();
+    }
+
+    public static long getRandomLongBetween0AndN(long n)
+    {
+        return ThreadLocalRandom.current().nextLong(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRandomIntegerBetweenNumbers(30,50));
+        System.out.println(getRandomDouble());
+        System.out.println(getRandomLongBetween0AndN(100000000));
+    }
+}
