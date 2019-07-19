4a7a9132456b69f8863e43fdc31e29c13eb560da
rossi23891 <rossi23891@gmail.com>
2019-07-19 14:32:18 +0400

    html red3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+    private View view;
+    //model
+    private HTMLDocument document;
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+        View currentView = new View();
+        Controller controller = new Controller(currentView);
+        currentView.setController(controller);
+        currentView.init();
+        controller.init();
+    }
+
+    public void init(){
+
+    }
+
+    public void exit(){
+        System.exit(0);
+    }
+}
