93dddd9725636bd336cbdb47367f52696127bf51
rossi23891 <rossi23891@gmail.com>
2019-07-22 18:01:14 +0300

    HTML editor 4

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,11 +1,19 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+    //панель с двумя вкладками
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    // компонент для визуального редактирования html.размещен на первой вкладке
+    private JTextPane htmlTextPane = new JTextPane();
+    //компонент для редактирования html в виде текста, он будет отображать код html
+    private JEditorPane plainTextPane = new JEditorPane();
 
     public Controller getController() {
         return controller;
@@ -21,10 +29,26 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init(){
-
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
     }
 
     public void exit(){
         controller.exit();
+    }
+
+    public void initGui(){// иниц. графический интерфейс
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void initMenuBar(){// инициализация меню редактора
+
+    }
+
+    public void initEditor(){// инициализация панелей редактора
+
     }
 }
