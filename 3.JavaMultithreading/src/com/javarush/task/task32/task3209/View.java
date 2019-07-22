17ce68b3e08c753d0fc2ebd333f0d1f161bf64f5
rossi23891 <rossi23891@gmail.com>
2019-07-23 00:51:17 +0400

    html red6

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -52,6 +54,11 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor(){// инициализация панелей редактора
-
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.addTab("HTML",new JScrollPane(htmlTextPane));
+        tabbedPane.addTab("Текст",new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(400,300));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane,BorderLayout.CENTER);
     }
 }
