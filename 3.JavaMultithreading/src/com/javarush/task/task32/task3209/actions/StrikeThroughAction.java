8047f6e73628da3cdbd581aee9e21ac2d43207f3
rossi23891 <rossi23891@gmail.com>
2019-07-23 12:59:51 +0300

    HTML editor 8

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/StrikeThroughAction.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task32.task3209.actions;
+
+import javax.swing.text.StyledEditorKit;
+import java.awt.event.ActionEvent;
+
+public class StrikeThroughAction extends StyledEditorKit.StyledTextAction {
+
+    public StrikeThroughAction() {
+        super("");
+    }
+
+    //стиль Зачеркнутый
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task32.task3209.actions;
+
+import javax.swing.text.StyledEditorKit;
+import java.awt.event.ActionEvent;
+
+public class SubscriptAction extends StyledEditorKit.StyledTextAction {
+
+    public SubscriptAction() {
+        super("");
+    }
+
+    //стиль текста "Подстрочный знак".
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
