3cc3f9e45383200ab58a377c0d5e0c31f8ea73e3
rossi23891 <rossi23891@gmail.com>
2019-08-03 14:46:58 +0400

    restaurant 2/1 2 3

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,10 +1,7 @@
 package com.javarush.task.task27.task2712;
 
-import com.javarush.task.task27.task2712.kitchen.Dish;
-
 public class Restaurant {
     public static void main(String[] args) {
-        String ttt = Dish.allDishesToString();
-        System.out.println(ttt);
+
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -1,14 +1,17 @@
 package com.javarush.task.task27.task2712.kitchen;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
 import com.javarush.task.task27.task2712.Tablet;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Order {//должна быть информация, относящаяся к списку выбранных пользователем блюд
     private final Tablet tablet;
     protected List<Dish> dishes;
 
-    public Order(Tablet tablet) {
+    public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
+        dishes = ConsoleHelper.getAllDishesForOrder();
     }
 }
