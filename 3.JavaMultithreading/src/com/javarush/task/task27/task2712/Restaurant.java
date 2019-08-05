2717026aa037e013220188ac713d8d12a8e64a45
rossi23891 <rossi23891@gmail.com>
2019-08-05 13:06:13 +0400

    restaurant 4

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,12 +1,14 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(1);
         Cook cook = new Cook("Amigo");
         tablet.addObserver(cook);
+        cook.addObserver(new Waiter());
         tablet.createOrder();
         tablet.createOrder();
         tablet.createOrder();
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
@@ -3,7 +3,7 @@ package com.javarush.task.task27.task2712.kitchen;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Cook implements Observer {
+public class Cook extends Observable implements Observer {
     private String name;
 
     public Cook(String name) {
@@ -15,8 +15,11 @@ public class Cook implements Observer {
         return name;
     }
 
+
     @Override
     public void update(Observable o, Object order) {
         System.out.println("Start cooking - " + order);
+        setChanged();
+        notifyObservers(order);
     }
 }
