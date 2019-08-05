ca650bcc25c16bb625329ce2e5db30e24782b166
rossi23891 <rossi23891@gmail.com>
2019-08-05 19:46:01 +0400

    restaurant 10

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -8,13 +8,18 @@ public class Advertisement {
     private int duration; //продолжительность в секундах
     private long amountPerOneDisplaying;
 
+
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
         this.name = name;
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
-        this.amountPerOneDisplaying = initialAmount/hits;
+        this.amountPerOneDisplaying = hits > 0 ? initialAmount / hits : 0;
+    }
+
+    public int getHits() {
+        return hits;
     }
 
     public String getName() {
@@ -29,9 +34,9 @@ public class Advertisement {
         return amountPerOneDisplaying;
     }
 
-    public void revalidate(){
-        if(hits<1){
-            throw new UnsupportedOperationException();
+    public void revalidate() {
+        if (hits <= 0) {
+            throw new NoVideoAvailableException();
         }
         hits--;
     }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task27.task2712.ad;
 
+
+import com.javarush.task.task27.task2712.ConsoleHelper;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class AdvertisementManager {
@@ -9,36 +14,99 @@ public class AdvertisementManager {
     final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;// time of cooking in SECONDS
     private long bestPrice;
+    private List<Advertisement> bestItems;
 
     public AdvertisementManager(int timeSeconds) {
         this.timeSeconds = timeSeconds;
     }
-    public void processVideos(){
-        if(storage.list().isEmpty()){
+
+    public void processVideos() {
+        if (storage.list().isEmpty()) {
             throw new NoVideoAvailableException();
+        }
+        List<Advertisement>checkAvailableHits =new ArrayList<>();
+        for(Advertisement ad: storage.list()){
+            if(ad.getHits()>0){
+                checkAvailableHits.add(ad);
+            }
+        }
+        List<Advertisement> videosToShow = findMostValuableAd(checkAvailableHits);
+        Comparator<Advertisement> comparatorVideo = new Comparator<Advertisement>() {
+            @Override
+            public int compare(Advertisement o1, Advertisement o2) {
+                long result = o2.getAmountPerOneDisplaying()-o1.getAmountPerOneDisplaying();
+                if(result!=0){
+                    return (int)result;
+                }else{
+                    return (int)((o1.getAmountPerOneDisplaying()/o1.getDuration()*1000) - (int)(o2.getAmountPerOneDisplaying()/o2.getDuration()*1000));
+                }                 
+            }
+        };
+        Collections.sort(videosToShow,comparatorVideo);
+        for (Advertisement ad : videosToShow) {
+            ConsoleHelper.writeMessage(ad.getName() + " is displaying... " + ad.getAmountPerOneDisplaying() + ", " + ad.getAmountPerOneDisplaying()*1000/(long)ad.getDuration());
+            ad.revalidate();
+        }
+
+    }
+
+    public List<Advertisement> findMostValuableAd(List<Advertisement> totalVideos) {// лучший вариант
+        findAllSets(totalVideos);
+        return bestItems;
+    }
+
+    public void findAllSets(List<Advertisement> advertisements) {//найти все возм варианты
+        if (advertisements.size() > 0) {
+            checkIfBestSet(advertisements);
+        }
+        for (int i = 0; i < advertisements.size(); i++) {
+            List<Advertisement> newSet = new ArrayList<>(advertisements);
+            newSet.remove(i);
+            findAllSets(newSet);
         }
     }
 
-    public List<Advertisement> findMostValuableAd(List<Advertisement> totalVideos,int timeSeconds){
-        return null;
-    }
-
-    private int calculateTolalDuration(List<Advertisement> advertisements){// вычисляем продолжительноть набора видео
+    private int calculateTolalDuration(List<Advertisement> advertisements) {// вычисляем продолжительноть набора видео
         int totalDuration = 0;
         for (Advertisement advertisement : advertisements) {
-            totalDuration+=advertisement.getDuration();
+            totalDuration += advertisement.getDuration();
         }
         return totalDuration;
     }
 
-    private long calculateTotalAmount(List<Advertisement> advertisements){//вычиялет общую стоимость набора видео
-        long totalAmount=0;
+    private long calculateTotalAmount(List<Advertisement> advertisements) {//вычиялет общую стоимость набора видео
+        long totalAmount = 0;
         for (Advertisement advertisement : advertisements) {
-            totalAmount+=advertisement.getAmountPerOneDisplaying();
+            totalAmount += advertisement.getAmountPerOneDisplaying();
         }
         return totalAmount;
     }
 
+    private void checkIfBestSet(List<Advertisement> advertisements) {
+        if (bestItems == null) {
+            if (calculateTolalDuration(advertisements) <= timeSeconds) {
+                bestItems = advertisements;
+                bestPrice = calculateTotalAmount(advertisements);
+            }
+        } else {
+            if (calculateTolalDuration(advertisements) <= timeSeconds) {
+                if (calculateTotalAmount(advertisements) > bestPrice) {
+                    bestItems = advertisements;
+                    bestPrice = calculateTotalAmount(advertisements);
+                }else if(calculateTotalAmount(advertisements) == bestPrice){
+                    if(calculateTolalDuration(bestItems)<calculateTolalDuration(advertisements)){
+                        bestItems = advertisements;
+                        bestPrice = calculateTotalAmount(advertisements);
+                    }else if(calculateTolalDuration(bestItems)==calculateTolalDuration(advertisements)){
+                        if(advertisements.size()<bestItems.size()){
+                            bestItems = advertisements;
+                            bestPrice = calculateTotalAmount(advertisements);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
 }
