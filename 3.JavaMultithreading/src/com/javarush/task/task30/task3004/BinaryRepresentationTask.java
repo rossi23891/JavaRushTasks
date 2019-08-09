e6d8767c35db01a4e35e904b307fa2094377a9ac
rossi23891 <rossi23891@gmail.com>
2019-08-09 15:48:26 +0400

    3304

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3004/BinaryRepresentationTask.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task30.task3004;
+
+import java.util.concurrent.RecursiveTask;
+
+public class BinaryRepresentationTask extends RecursiveTask<String> {
+    private int x;
+
+    public BinaryRepresentationTask(int x) {
+        this.x = x;
+    }
+
+    @Override
+    protected String compute() {
+        if (x==0)
+            return "";
+        int a = x % 2;
+        int b = x / 2;
+
+        if(b>0){
+            BinaryRepresentationTask task = new BinaryRepresentationTask(b);
+            task.fork();
+            return task.join() + a;
+        }else{
+            return String.valueOf(a);
+        }
+
+    }
+}
