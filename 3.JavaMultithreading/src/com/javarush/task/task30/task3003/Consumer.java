2aa02def28fba35f6e352b0e6e9bdeb8d43b468f
rossi23891 <rossi23891@gmail.com>
2019-08-09 12:58:42 +0400

    3003

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Consumer.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task30.task3003;
+
+import java.util.concurrent.TransferQueue;
+
+public class Consumer implements Runnable {
+
+    private TransferQueue<ShareItem> queue;
+
+    public Consumer(TransferQueue<ShareItem> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(450);
+        } catch (InterruptedException e) {
+        }
+        while (true){
+            try {
+                ShareItem item =queue.take();
+                System.out.format("Processing %s"+ item.toString());
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Producer.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task30.task3003;
+
+import java.util.concurrent.TransferQueue;
+
+public class Producer implements Runnable {
+
+    private TransferQueue<ShareItem> queue;
+
+    public Producer(TransferQueue<ShareItem> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 10; i++) {
+            System.out.format("Элемент 'ShareItem-%d' добавлен%n", i);
+            queue.offer(new ShareItem("ShareItem-" + i,i));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+            if(queue.hasWaitingConsumer()){
+                System.out.format("Consumer в ожидании!");
+            }
+        }
+    }
+}
