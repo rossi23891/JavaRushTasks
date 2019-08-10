b2de292947f50234a1881e1fbcb2caf79c70f2b2
rossi23891 <rossi23891@gmail.com>
2019-08-10 23:50:20 +0400

    game 2048 level 30 1-2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Tile.java
@@ -0,0 +1,72 @@
+package com.javarush.task.task35.task3513;
+
+import java.awt.*;
+
+public class Tile {//плитка.Кроме веса у плитки еще будет собственный цвет и цвет текста которым будет отображаться вес плитки.
+    //Цвета плиток находятся в диапазоне от светло-серого до красного, а цвет текста будет зависеть от цвета плитки
+    int value;
+
+    public Tile() {
+    }
+
+    public Tile(int value) {
+        this.value = value;
+    }
+
+    public boolean isEmpty() {
+        return value == 0;
+    }
+
+    public Color getFontColor() {
+        if (value < 16) {
+            return new Color(0x776e65);
+        }
+        return new Color(0xf9f6f2);
+    }
+
+    public Color getTileColor() {
+        Color color;
+        switch (value) {
+            case 0:
+                color = new Color(0xcdc1b4);
+                break;
+            case 2:
+                color = new Color(0xeee4da);
+                break;
+            case 4:
+                color = new Color(0xede0c8);
+                break;
+            case 8:
+                color = new Color(0xf2b179);
+                break;
+            case 16:
+                color = new Color(0xf59563);
+                break;
+            case 32:
+                color = new Color(0xf67c5f);
+                break;
+            case 64:
+                color = new Color(0xf65e3b);
+                break;
+            case 128:
+                color = new Color(0xedcf72);
+                break;
+            case 256:
+                color = new Color(0xedcc61);
+                break;
+            case 512:
+                color = new Color(0xedc850);
+                break;
+            case 1024:
+                color = new Color(0xedc53f);
+                break;
+            case 2048:
+                color = new Color(0xedc22e);
+                break;
+            default:
+                color = new Color(0xff0000);
+                break;
+        }
+        return color;
+    }
+}
