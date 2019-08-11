c00cfc500429bf487bf9fabe3756a5385bb00c69
rossi23891 <rossi23891@gmail.com>
2019-08-11 20:54:29 +0400

    2048 level 30 task 5

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Model {// game field manipulations
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles=new Tile[FIELD_WIDTH][FIELD_WIDTH];
+    public int score;
+    public int maxTile;
 
     public Model() {
         resetGameTiles();
@@ -17,6 +19,8 @@ public class Model {// game field manipulations
                 gameTiles[row][col] = new Tile();
             }
         }
+        score=0;
+        maxTile=0;
         addTile();
         addTile();
     }
@@ -39,5 +43,41 @@ public class Model {// game field manipulations
             Tile toBeAdded = emptyTiles.get((int) (emptyTiles.size() * Math.random()));
             toBeAdded.setValue(Math.random() < 0.9 ? 2 : 4);
         }
+    }
+
+    private boolean compressTiles(Tile[] tiles){
+        boolean isCompressed = false;
+        for (int i = 0; i < tiles.length-1; i++) {
+            if(tiles[i].value==0){
+                for (int j = i+1; j < tiles.length; j++) {
+                    if(tiles[j].value!=0){
+                        tiles[i].setValue(tiles[j].value);
+                        tiles[j].value=0;
+                        isCompressed=true;
+                        break;
+                    }
+                }
+            }
+        }
+        return isCompressed;
+    }
+
+    private boolean mergeTiles(Tile[] tiles){
+        boolean isMerged = false;
+        for (int i = 0; i < tiles.length-1; i++) {
+            if(tiles[i].value!=0){
+                if(tiles[i].value==tiles[i+1].value){
+                    tiles[i].setValue(tiles[i].value+tiles[i+1].value);
+                    tiles[i+1].setValue(0);
+                    score+=tiles[i].value;
+                    if(tiles[i].value>maxTile){
+                       maxTile = tiles[i].value;
+                    }
+                    isMerged=true;
+                }
+            }
+        }
+        compressTiles(tiles);
+        return isMerged;
     }
 }
