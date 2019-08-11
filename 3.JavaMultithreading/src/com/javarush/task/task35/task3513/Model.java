a58e1f629eb31f86ff198d5afbf23f0b137a136a
rossi23891 <rossi23891@gmail.com>
2019-08-11 20:05:12 +0400

    4 1/2

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {// game field manipulations
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles=new Tile[FIELD_WIDTH][FIELD_WIDTH];
@@ -10,5 +13,21 @@ public class Model {// game field manipulations
                 gameTiles[row][col] = new Tile();
             }
         }
+    }
+
+    private List<Tile> getEmptyTiles(){
+        List<Tile> emptyTiles = new ArrayList<>();
+        for (int row = 0; row <gameTiles.length ; row++) {
+            for (int col = 0; col < gameTiles[0].length; col++) {
+                if(gameTiles[row][col].value==0){
+                    emptyTiles.add(gameTiles[row][col]);
+                }
+            }
+        }
+        return emptyTiles;
+    }
+
+    private void addTile(List<Tile> emptyTiles){
+        Tile toBeAdded = emptyTiles.get((int)(emptyTiles.size()*Math.random()));
     }
 }
