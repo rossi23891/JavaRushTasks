2acae7a9c0df9e1d4d7461aca2519038d28ad49a
rossi23891 <rossi23891@gmail.com>
2019-08-11 19:52:35 +0400

    game 2048 level 30 3

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task35.task3513;
 
-public class Model {
+public class Model {// game field manipulations
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles=new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model() {
+        for (int row = 0; row <gameTiles.length ; row++) {
+            for (int col = 0; col < gameTiles[0].length; col++) {
+                gameTiles[row][col] = new Tile();
+            }
+        }
+    }
 }
