b74743bb92c0ac320571d0a704812338a400d229
rossi23891 <rossi23891@gmail.com>
2019-08-14 18:42:58 +0400

    3112

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task31.task3112;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/* 
+Загрузчик файлов
+*/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("D:/MyDownloads"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        // implement this method
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+        Path tempFile = Files.createTempFile("temp-" , ".tmp");
+        Files.copy(inputStream,tempFile);
+        return Files.move(tempFile,downloadDirectory.resolve(Paths.get(urlString).getFileName()),StandardCopyOption.REPLACE_EXISTING);
+    }
+}
