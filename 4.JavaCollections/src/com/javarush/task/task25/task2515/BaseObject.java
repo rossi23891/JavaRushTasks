70dc326c9174d7a13a3965b497cd28b921a5ab06
rossi23891 <rossi23891@gmail.com>
2019-09-26 17:50:39 +0300

    space 6

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task25.task2515;
+
+public abstract class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+    private boolean isAlive;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        isAlive=true;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void draw(){
+
+    }
+
+    public  void move(){
+
+    }
+
+    public void die(){
+        isAlive=false;
+    }
+
+    //пересеклись объекты или нет
+    public boolean isIntersect(BaseObject o){
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
+
+}
