291b5252a906af87f0dc418c9c6162c63b0ee78d
rossi23891 <rossi23891@gmail.com>
2020-05-04 23:29:44 +0300

    aggregator till 15

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,13 +3,20 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView  implements View{
     private Controller controller;
+    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName()
+            .replace(".","/") + "/vacancies.html";
+
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        String updatedFileContent = getUpdatedFileContent(vacancies);
+        updateFile(updatedFileContent);
     }
 
     @Override
@@ -19,5 +26,17 @@ public class HtmlView  implements View{
 
     public void userCitySelectEmulationMethod(){
         controller.onCitySelect("Odessa");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancyList){
+        return null;
+    }
+
+    private void updateFile(String fileContent){
+        try (FileWriter fileWriter = new FileWriter(new File(filePath))) {
+            fileWriter.write(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
